9c3ccb6251a80f0da26f897d47c0b65c9d7398e4
ToxiPanda <121240392+ToxiPanda@users.noreply.github.com>
2023-04-22 17:28:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "dUppMVl_QUUENVIC_6Mm4=",
   LANG: process.env.LANG || "EN",
   HANDLERS:
     process.env.HANDLER === "false" || process.env.HANDLER === "null"
